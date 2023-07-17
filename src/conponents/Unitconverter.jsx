78e3fc924fa2f47d9e8214437b299d39d0fd12a9
Arslan choudary <arslanchoudary354@gmail.com>
2023-07-17 13:17:07 +0500

    new commit

--- src/conponents/Unitconverter.jsx
+++ src/conponents/Unitconverter.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Unitconverter() {
   return (
     <div className='unitbg'>
-      converter page
+      <h1 className='text-white'>This page is temporary Empty</h1>
     </div>
   )
 }
